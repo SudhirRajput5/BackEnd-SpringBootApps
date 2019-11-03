42e6594bbf0a5e3871e0b76f7b7028210b8e7677
unknown <sudhirrajput24@gmail.com>
2019-11-04 01:45:50 +0530

    adding logger project

--- SprintBootAppWithController/SpringBootWithControllerRunner/src/main/java/com/sudhir/SrpingWithController/Controller/userController.java
+++ SprintBootAppWithController/SpringBootWithControllerRunner/src/main/java/com/sudhir/SrpingWithController/Controller/userController.java
@@ -1,5 +1,8 @@
 package com.sudhir.SrpingWithController.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")  //http://localhost:8080/users
 public class userController {
 
+	Logger logger = LoggerFactory.getLogger(userController.class);
+	
+	@Value("${spring.application.name:SudhirRajputApplication}")
+	private String name;
+	
 	@GetMapping
 	@RequestMapping("/getusser")
 	public String getUser(){
-		return "getuserrestcall";
+		logger.error("This is a logging example");
+		return name;
+		
 	}
+	
 	
 	@PostMapping
 	public String createUser() {
