fa3e898092cb469410d7296bc62f94f81b3c9472
unknown <sudhirrajput24@gmail.com>
2019-11-07 17:18:03 +0530

    adding project

--- SprintBootAppWithController/SpringBootWithControllerJPA/src/main/java/com/sudhir/SrpingWithControllerJPA/SpringBootWithControllerJpaApplication.java
+++ SprintBootAppWithController/SpringBootWithControllerJPA/src/main/java/com/sudhir/SrpingWithControllerJPA/SpringBootWithControllerJpaApplication.java
@@ -2,27 +2,13 @@ package com.sudhir.SrpingWithControllerJPA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-import com.sudhir.SrpingWithControllerJPA.service.UserService;
-import com.sudhir.SrpingWithControllerJPA.shared.dto.UserDto;
-
-@SpringBootApplication(scanBasePackages="com.sudhir.SrpingWithControllerJPA.service")
+@SpringBootApplication
 public class SpringBootWithControllerJpaApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication
-				.run(SpringBootWithControllerJpaApplication.class, args);
+		SpringApplication.run(SpringBootWithControllerJpaApplication.class, args);
 
-		UserService userService = applicationContext.getBean("userService", UserService.class);
-
-		UserDto userDto = new UserDto();
-		userDto.setEmail("testing@gmail.com");
-		userDto.setFirstName("Abhinav");
-		userDto.setLastName("kumar");
-		userDto.setPassword("HIIIIII");
-		userService.createUser(userDto);
-	}
+		}
 
 }
